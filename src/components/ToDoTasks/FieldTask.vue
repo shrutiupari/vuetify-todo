bc55040fc7d3b4b0c24e4357c7a9dc7e3166d462
Shruti <shrutiupari@users.noreply.github.com>
2022-06-05 22:40:02 +0530

    Create FieldTask.vue

---
+++ src/components/ToDoTasks/FieldTask.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-text-field
+    v-model="newTaskTitle"
+    @click:append="addTask"
+    @keyup.enter="addTask"
+    class="pa-3"
+    label="Regular"
+    append-icon="mdi-plus"
+    outlined
+    hide-details
+    clearable
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTaskTitle: "",
+      tasks: [
+        // {
+        //   id: 1,
+        //   title: "Notifications",
+        //   done: false,
+        // },
+        // {
+        //   id: 2,
+        //   title: "Alarm",
+        //   done: false,
+        // },
+        // {
+        //   id: 3,
+        //   title: "Study",
+        //   done: false,
+        // },
+      ],
+    };
+  },
+  methods: {
+    addTask() {
+      this.$store.dispatch("addTask", this.newTaskTitle);
+      this.newTaskTitle = "";
+    },
+  },
+};
+</script>
+
+<style></style>
