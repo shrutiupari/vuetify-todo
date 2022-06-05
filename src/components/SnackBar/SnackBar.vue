da68be6e52c20be8db6536918eb5c8a756377f6c
Shruti <shrutiupari@users.noreply.github.com>
2022-06-05 22:40:54 +0530

    Create SnackBar.vue

---
+++ src/components/SnackBar/SnackBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-snackbar v-model="$store.state.snackbar.show">
+    {{ $store.state.snackbar.text }}
+
+    <template v-slot:action="{ attrs }">
+      <v-btn text v-bind="attrs" @click="$store.commit('hideSnackBar')">
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    snackbar: false,
+    text: `Task Added`,
+  }),
+};
+</script>
+
+<style></style>
