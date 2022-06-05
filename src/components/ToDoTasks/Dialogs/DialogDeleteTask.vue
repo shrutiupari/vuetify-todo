4757150aa9f68e08806cd35f21d514420bc94f12
Shruti <shrutiupari@users.noreply.github.com>
2022-06-05 23:08:55 +0530

    Create DialogDeleteTask.vue

---
+++ src/components/ToDoTasks/Dialogs/DialogDeleteTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog :value="true" persistent max-width="290">
+    <v-card>
+      <v-card-title class="text-h5">
+        Delete Task?
+      </v-card-title>
+      <v-card-text
+        >Are you sure?</v-card-text
+      >
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="$emit('close')">
+          No
+        </v-btn>
+        <v-btn color="red darken-1" text @click="$store.dispatch('deleteTask', task.id)">
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['task']
+};
+</script>
+
+<style></style>
